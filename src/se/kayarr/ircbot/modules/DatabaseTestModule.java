72c7151db101d9f0475322f0f53dd2ad65f05660
Raymond Hammarling <raykay.rh@gmail.com>
2013-06-24 21:04:12 +0200

    Further added to the database test module

--- src/se/kayarr/ircbot/modules/DatabaseTestModule.java
+++ src/se/kayarr/ircbot/modules/DatabaseTestModule.java
@@ -1,11 +1,42 @@
 package se.kayarr.ircbot.modules;
 
-import se.kayarr.ircbot.backend.Module;
+import org.pircbotx.Channel;
+import org.pircbotx.PircBotX;
+import org.pircbotx.User;
 
-public class DatabaseTestModule extends Module {
+import se.kayarr.ircbot.backend.CommandHandler;
+import se.kayarr.ircbot.backend.CommandManager;
+import se.kayarr.ircbot.backend.Module;
+import se.kayarr.ircbot.database.Database;
+import se.kayarr.ircbot.database.Table;
+import se.kayarr.ircbot.database.TableHandler;
+
+public class DatabaseTestModule extends Module implements CommandHandler, TableHandler {
+	
+	private Table testTable;
 
 	@Override
 	public void initialize() {
+		
+		CommandManager.get().newCommand()
+			.addAlias("dbtest")
+			.handler(this)
+			.finish();
+		
+		testTable = Database.moduleData().table("test_table", 1, this);
+	}
+	
+	@Override
+	public void onTableCreate(Table table) {
+	}
+	
+	@Override
+	public void onTableUpgrade(Table table, int oldVersion, int newVersion) {
+	}
+	
+	@Override
+	public void onHandleCommand(PircBotX bot, Channel channel, User user,
+			String command, String parameters) {
 	}
 
 }
