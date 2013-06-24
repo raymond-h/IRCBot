27caa9b8ceb4f24b5f140054e1ddacf49cb9016e
Raymond Hammarling <raykay.rh@gmail.com>
2013-06-24 20:57:52 +0200

    Started implementation of Database and Table

--- src/se/kayarr/ircbot/database/Database.java
+++ src/se/kayarr/ircbot/database/Database.java
@@ -1,5 +1,43 @@
 package se.kayarr.ircbot.database;
 
-public class Database {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
+import lombok.Getter;
+
+public class Database {
+	@Getter private String name;
+	private Connection conn;
+	
+	public Database(String name) {
+		this.name = name;
+	}
+	
+	public void connect() throws SQLException {
+		if(conn == null) {
+			
+			conn = DriverManager.getConnection("jdbc:h2:"+name+";create=true");
+		}
+	}
+	
+	public static Database connectToDatabase(String dbName) throws SQLException {
+		Database db = new Database(dbName);
+		db.connect();
+		return db;
+	}
+	
+	public Table table(String name, int version, TableHandler handler) {
+		return new Table(this, name, version, handler);
+	}
+	
+	//TODO Temporary implementation
+	public boolean tableExists(String name) {
+		return false;
+	}
+	
+	//TODO Temporary implementation
+	int tableVersion(String name) {
+		return 1;
+	}
 }
--- src/se/kayarr/ircbot/database/Table.java
+++ src/se/kayarr/ircbot/database/Table.java
@@ -1,5 +1,32 @@
 package se.kayarr.ircbot.database;
 
-public class Table {
+import lombok.Getter;
 
+public class Table {
+	@Getter private Database owner;
+	
+	@Getter private String name;
+	@Getter private int version;
+	
+	Table(Database owner, String name, int version, TableHandler handler) {
+		this.owner = owner;
+		this.name = name;
+		this.version = version;
+		
+		if(handler != null) {
+			if(!owner.tableExists(name)) {
+				//Create table
+				handler.onTableCreate(this);
+			}
+			else {
+				int oldVersion = owner.tableVersion(name);
+				
+				if(oldVersion < version) {
+					//Upgrade table
+					handler.onTableUpgrade(this, oldVersion, version);
+				}
+			}
+
+		}
+	}
 }
