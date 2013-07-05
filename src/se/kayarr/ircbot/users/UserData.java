60f1dbbafb8eacf79b21628b988e965d5e4c9e68
Raymond Hammarling <raykay.rh@gmail.com>
2013-07-05 04:42:38 +0200

    Made UserData a Serializable, updated JSON serialization
    When serialized to JSON (using GSON), UserData is serialized using
    ObjectOutputStream and converted to Base64, then embedded as a string

--- src/se/kayarr/ircbot/users/UserData.java
+++ src/se/kayarr/ircbot/users/UserData.java
@@ -1,67 +1,61 @@
 package se.kayarr.ircbot.users;
 
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.io.StringReader;
+import java.io.StringWriter;
 import java.lang.reflect.Type;
 import java.util.HashMap;
 import java.util.Map;
 
+import com.google.common.io.BaseEncoding;
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonNull;
 import com.google.gson.JsonParseException;
+import com.google.gson.JsonPrimitive;
 import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonSerializer;
-import com.google.gson.reflect.TypeToken;
 
-public class UserData {
-	private Map<String, Object> data = new HashMap<>();
+public class UserData implements Serializable {
+
+	private static final long serialVersionUID = 8323882601258076215L;
 	
-	public void put(String key, int value) {
-		put(key, Integer.valueOf(value));
-	}
+	private Map<String, Serializable> data = new HashMap<>();
 	
-	private void put(String key, Number value) {
+	public void put(String key, Serializable value) {
 		data.put(key, value);
 	}
 	
-	public void put(String key, String value) {
-		data.put(key, value);
-	}
-	
-	public int getInt(String key) {
-		return getNumber(key).intValue();
-	}
-	
-	private Number getNumber(String key) {
-		return (Number) data.get(key);
-	}
-	
-	public String getString(String key) {
-		return (String) data.get(key);
+	public Serializable get(String key) {
+		return data.get(key);
 	}
 	
 	public boolean has(String key) {
 		return data.containsKey(key);
 	}
 	
-	public UserData defaultValue(String key, int value) {
+	public int getInt(String key) {
+		return (Integer) get(key);
+	}
+	
+	public String getString(String key) {
+		return (String) get(key);
+	}
+	
+	public <T extends Serializable> T getAs(String key, Class<T> clazz) {
+		return clazz.cast(get(key));
+	}
+	
+	public UserData defaultValue(String key, Serializable value) {
 		setDefaultValue(key, value);
 		return this;
 	}
 	
-	public UserData defaultValue(String key, String value) {
-		setDefaultValue(key, value);
-		return this;
-	}
-	
-	public boolean setDefaultValue(String key, int value) {
-		return setDefaultValueImpl(key, value);
-	}
-	
-	public boolean setDefaultValue(String key, String value) {
-		return setDefaultValueImpl(key, value);
-	}
-	
-	private boolean setDefaultValueImpl(String key, Object value) {
+	public boolean setDefaultValue(String key, Serializable value) {
 		if(has(key)) return false;
 		
 		data.put(key, value);
@@ -74,17 +68,45 @@ public class UserData {
 		public UserData deserialize(JsonElement json, Type typeOfT,
 				JsonDeserializationContext context) throws JsonParseException {
 			
-			UserData inst = new UserData();
-			Type dataType = new TypeToken< Map<String,Object> >(){}.getType();
-			inst.data = context.deserialize(json, dataType);
-			return inst;
+			try {
+				String base64str = json.getAsString();
+				
+				StringReader reader = new StringReader(base64str);
+				ObjectInputStream in = new ObjectInputStream( BaseEncoding.base64().decodingStream(reader) );
+				
+				UserData o = (UserData) in.readObject();
+				
+				in.close();
+				reader.close();
+				
+				return o;
+			}
+			catch (ClassNotFoundException | IOException e) {
+				e.printStackTrace();
+			}
+			
+			return null;
 		}
 
 		@Override
 		public JsonElement serialize(UserData src, Type typeOfSrc,
 				JsonSerializationContext context) {
 			
-			return context.serialize(src.data);
+			try {
+				StringWriter writer = new StringWriter();
+				ObjectOutputStream out = new ObjectOutputStream( BaseEncoding.base64().encodingStream(writer) );
+				out.writeObject(src);
+				
+				out.close();
+				writer.close();
+				
+				return new JsonPrimitive(writer.toString());
+			}
+			catch (IOException e) {
+				e.printStackTrace();
+				
+				return JsonNull.INSTANCE;
+			}
 		}
 		
 	}
